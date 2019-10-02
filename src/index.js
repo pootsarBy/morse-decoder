6feee631124ba6e175946093de9dee1c06847139
Mikhail Rybinski <pootsar@gmail.com>
2019-10-02 18:49:05 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,38 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let arr = expr.match(/.{1,2}/g);
+    message = [];
+    for (let i=0; i<arr.length; i++)
+    {
+        if (arr[i] == '00'){
+            arr[i] = '';
+        }
+        else if (arr[i] == '10'){
+            arr[i] = '.';
+        }
+        else if(arr[i] == '11'){
+            arr[i] = '-';
+        }
+        else if(arr[i] == '**'){
+            arr[i] = ' ';
+           // arr.splice(i+1, 4);
+        }
+    } 
+    for (let i=5; i<arr.length; i+=6)
+    arr.splice(i,0," ");
+    arr = arr.join("");
+    console.log(arr);
+    arr.split("   ").map(function (word) {
+        word.split(" ").map(function (letter) {
+            message.push(MORSE_TABLE[letter]);
+        });
+        message.push(" ");
+    });
+    message.pop();
+    message = message.join("").replace(/\s{2,}/g, ' ');
+    console.log(message+'length= '+message.length);
+    return(message);
 }
 
 module.exports = {
